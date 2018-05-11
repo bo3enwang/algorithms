0c63566198aa25d884a2d9076bff5a49404b2c87
bow3n <pioneerwbw@gmail.com>
2018-05-11 23:32:15 +0800

    Insertion sort

---
+++ src/main/java/com/bow3n/learn/algorithms/ch02/Insertion.java
@@ -0,0 +1,23 @@
+package com.bow3n.learn.algorithms.ch02;
+
+public class Insertion {
+
+    public void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j > 0 && less(a[j], a[i]); j--) {
+                exch(a, j, j - 1);
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
